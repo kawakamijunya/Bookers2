d8de14efa8521396db7e1bd6f24290fe879e49cf
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2022-12-14 06:08:17 +0000

    [Add]4-27user.index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @user=current_user
+    @book=Book.new
+    @users=User.all
   end
 
   def show
+    
   end
 
   def edit
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:introduction)
+  end
+
 end
