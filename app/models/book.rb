e1cd678faad4c439a5924089afe96c57c83bf569
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2022-12-16 05:51:43 +0000

    [Add]4-27フラッシュメッセージ、バリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
   has_one_attached :profile_image
   belongs_to :user
+  
+  validates :title, presence: true
+  validates :body, presence: true
 
 
   def get_profile_image
